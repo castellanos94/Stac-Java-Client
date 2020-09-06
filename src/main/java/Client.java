5b1957df9b6192d0ad72d77a684bdfd6221dd9e1
aca810@gmail.com <aca810@gmail.com>
2020-09-05 21:20:14 -0500

    Non-parametric multiple groups All vs All

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -26,7 +26,10 @@ public class Client {
     /**
      * <bold>D'Agostino-Pearson</bold>
      * It's less powerful than Shapiro-Wilk.
-     * <bold>Type: Normality Test</bold>
+     * <p>
+     * <bold>Type: Parametric Test</bold>
+     * <p>
+     * <bold>Subtype: Normality Test</bold>
      * <p>
      *
      * @param path_csv_file      load data
@@ -43,7 +46,10 @@ public class Client {
      * <bold>Shapiro-Wilk</bold>
      * The best of the three methods, especially for samples of less than 30 elements.
      * <p>
-     * <bold>Type: Normality Test</bold>
+     * <bold>Type: Parametric Test</bold>
+     * <p>
+     * <bold>Subtype: Normality Test</bold>
+     * <p>
      *
      * @param path_csv_file      load data
      * @param significance_level Probability of rejecting a null hypothesis when it is true.
@@ -60,7 +66,10 @@ public class Client {
      * <bold>Kolmogorov-Smirnov</bold>
      * The less powerful.
      * <p>
-     * <bold>Type: Normality Test</bold>
+     * <bold>Type: Parametric Test</bold>
+     * <p>
+     * <bold>Subtype: Normality Test</bold>
+     * <p>
      *
      * @param path_csv_file      load data
      * @param significance_level Probability of rejecting a null hypothesis when it is true.
@@ -72,6 +81,158 @@ public class Client {
         return getMap(path_csv_file, end_point);
     }
 
+    /**
+     * <bold>Friedman</bold>
+     * This test makes comparisons and assigns rankings to each data set. The statistic follows a chis-quared
+     * distribution with K−1 degrees of freedom, being K the number of related variables (or number of algorithms).
+     * <p>
+     * <bold>Type: Non Parametric Test</bold>
+     * <p>
+     * <bold>Subtype: Ranking Test</bold>
+     * <p>
+     *
+     * @param path_csv_file      load data
+     * @param significance_level Probability of rejecting a null hypothesis when it is true.
+     *                           Also known as confidence level or Type I error (false positive)
+     * @return Map object response
+     */
+    public static Map FRIEDMAN(String path_csv_file, double significance_level) {
+        return getMap(path_csv_file, "friedman/finner_multitest/" + significance_level);
+    }
+
+    /**
+     * <bold>Friedman Aligned Ranks</bold>
+     * It makes comparisons an assigns rankings considering all the data sets. It is usually employed when the number
+     * of algorithms in the comparison is low
+     * <p>
+     * <bold>Type: Non Parametric Test</bold>
+     * <p>
+     * <bold>Subtype: Ranking Test</bold>
+     * <p>
+     *
+     * @param path_csv_file      load data
+     * @param significance_level Probability of rejecting a null hypothesis when it is true.
+     *                           Also known as confidence level or Type I error (false positive)
+     * @return Map object response
+     */
+    public static Map FRIEDMAN_ALIGNED_RANK(String path_csv_file, double significance_level) {
+        return getMap(path_csv_file, "friedman-aligned-ranks/finner_multitest/" + significance_level);
+    }
+
+
+    /**
+     * <bold>Quade</bold>
+     * Similar to ImanDavenport, only that it takes into account that some problems are more difficult or that the
+     * results obtained from different algorithms present higher discrepancies (weighting).
+     * <p>
+     * <bold>Type: Non Parametric Test</bold>
+     * <p>
+     * <bold>Subtype: Ranking Test</bold>
+     * <p>
+     *
+     * @param path_csv_file      load data
+     * @param significance_level Probability of rejecting a null hypothesis when it is true.
+     *                           Also known as confidence level or Type I error (false positive)
+     * @return Map object response
+     */
+    public static Map QUADE(String path_csv_file, double significance_level) {
+        return getMap(path_csv_file, "quade/finner_multitest/" + significance_level);
+
+    }
+
+    /**
+     * <bold>Nemenyi</bold>
+     * <p>
+     * <bold>Type: Non Parametric Test</bold>
+     * <p>
+     * <bold>Subtype: Post-hoc Test</bold>
+     * <p>
+     *
+     * @param path_csv_file      load data
+     * @param significance_level Probability of rejecting a null hypothesis when it is true.
+     *                           Also known as confidence level or Type I error (false positive)
+     * @return Map object response
+     */
+    public static Map NEMENYI(String path_csv_file, double significance_level) {
+        return getMap(path_csv_file, "quade/nemenyi_multitest/" + significance_level);
+    }
+
+    /**
+     * <bold>Holm</bold>
+     * It compares each pi (starting from the most significant or the lowest) with: α(K−i), where i∈[1,K−1].
+     * If the hypothesis is rejected the test continues the comparisons. When an hypothesis is accepted, all the other hypothesis are accepted as well. It is better (more power) than Bonferroni-Dunn test, because it controls the FWER (familywise error rate), which is the probability of committing one or more type I errors among all hypothesis
+     * <p>
+     * <bold>Type: Non Parametric Test</bold>
+     * <p>
+     * <bold>Subtype: Post-hoc Test</bold>
+     * <p>
+     *
+     * @param path_csv_file      load data
+     * @param significance_level Probability of rejecting a null hypothesis when it is true.
+     *                           Also known as confidence level or Type I error (false positive)
+     * @return Map object response
+     */
+    public static Map HOLM(String path_csv_file, double significance_level) {
+        return getMap(path_csv_file, "quade/holm_multitest/" + significance_level);
+    }
+
+    /**
+     * <bold>Finner</bold>
+     * Finner's test is similar to Holm's but each p-value associated with the hypothesis Hi is compared with:
+     * pi≤1−(1−α)(K−1)i, where i∈[1,K−1]. It is more powerful than Bonferroni-Dunn, Holm, Hochberg and Li (only in some cases).
+     * <p>
+     * <bold>Type: Non Parametric Test</bold>
+     * <p>
+     * <bold>Subtype: Post-hoc Test</bold>
+     * <p>
+     *
+     * @param path_csv_file      load data
+     * @param significance_level Probability of rejecting a null hypothesis when it is true.
+     *                           Also known as confidence level or Type I error (false positive)
+     * @return Map object response
+     */
+    public static Map FINNER(String path_csv_file, double significance_level) {
+        return getMap(path_csv_file, "quade/finner_multitest/" + significance_level);
+    }
+
+    /**
+     * <bold>Hochberg</bold>
+     * It compares in the opposite direction to Holm. As soon as an acceptable hypothesis is found, all the other
+     * hypothesis are accepted. It is better (more power) than Holm test, but the differences between them are small in practice.
+     * <p>
+     * <bold>Type: Non Parametric Test</bold>
+     * <p>
+     * <bold>Subtype: Post-hoc Test</bold>
+     * <p>
+     *
+     * @param path_csv_file      load data
+     * @param significance_level Probability of rejecting a null hypothesis when it is true.
+     *                           Also known as confidence level or Type I error (false positive)
+     * @return Map object response
+     */
+    public static Map HOCHBERG(String path_csv_file, double significance_level) {
+        return getMap(path_csv_file, "quade/hochberg_multitest/" + significance_level);
+    }
+
+    /**
+     * <bold>Shaffer</bold>
+     * This test is like Holm's but each p-value associated with the hypothesis Hi is compared as pi≤αti, where ti is
+     * the maximum number of possible hypothesis assuming that the previous (j−1) hypothesis have been rejected.
+     * <p>
+     * <bold>Type: Non Parametric Test</bold>
+     * <p>
+     * <bold>Subtype: Post-hoc Test</bold>
+     * <p>
+     * <h1>it does not work</h1>
+     * @param path_csv_file      load data
+     * @param significance_level Probability of rejecting a null hypothesis when it is true.
+     *                           Also known as confidence level or Type I error (false positive)
+     * @return Map object response
+     */
+    @Deprecated
+    public static Map SHAFFTER(String path_csv_file, double significance_level) {
+        return getMap(path_csv_file, "friedman/shaffer_multitest/" + significance_level);
+    }
 
     private static String transform_to_json(String path_csv_file) throws IOException {
         Table table = Table.read().csv(path_csv_file);
@@ -82,6 +243,7 @@ public class Client {
         return gson.toJson(data);
 
     }
+
 
     private static Map getMap(String path_csv_file, String end_point) {
         try {
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         String path = "src/main/resources/Toxina-Friedman-min3.csv";
+        path = "src/main/resources/dtlz1.csv";
         System.out.println("Kolmogorov-Smirnov: " + Client.KOLMOGOROV(path, Client.SIGNIFICANCE_LEVEL[3]));
         System.out.println("Agostino: " + Client.AGOSTINO(path, Client.SIGNIFICANCE_LEVEL[3]));
         System.out.println("Shapiro: " + Client.SHAPIRO(path, Client.SIGNIFICANCE_LEVEL[3]));
+        System.out.println("Friedman: " + Client.FRIEDMAN(path, Client.SIGNIFICANCE_LEVEL[3]));
+        System.out.println("FriedmanA: " + Client.FRIEDMAN_ALIGNED_RANK(path, Client.SIGNIFICANCE_LEVEL[3]));
+
+        System.out.println("SHAFTER: " + Client.SHAFFTER(path, Client.SIGNIFICANCE_LEVEL[3]));
+
 
     }
 }
