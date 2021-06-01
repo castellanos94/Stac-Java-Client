1bc4133101190e7ff109389ceeaed0416a3908bf
castellanos94 <aca810@gmail>
2021-05-31 22:22:56 -0500

    added wilcoxon

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,19 +4,19 @@ public class Main {
     public static void main(String[] args) {
         String path = "src/main/resources/Toxina-Friedman-min3.csv";
         path = "src/main/resources/dtlz1.csv";
-        System.out.println("Kolmogorov-Smirnov: " + Client.KOLMOGOROV(path, Client.SIGNIFICANCE_LEVEL[3]));
-        System.out.println("Agostino: " + Client.AGOSTINO(path, Client.SIGNIFICANCE_LEVEL[3]));
-        System.out.println("Shapiro: " + Client.SHAPIRO(path, Client.SIGNIFICANCE_LEVEL[3]));
-        System.out.println("Friedman: " + Client.FRIEDMAN(path, Client.SIGNIFICANCE_LEVEL[3], POST_HOC.HOLM));
-        System.out.println("Friedman Aligned Ranks: " + Client.FRIEDMAN_ALIGNED_RANK(path, Client.SIGNIFICANCE_LEVEL[1], POST_HOC.HOCHBERG));
-        System.out.println("QUADE-NEMENYI: " + Client.QUADE(path, Client.SIGNIFICANCE_LEVEL[0], POST_HOC.NEMENYI));
-        System.out.println("QUADE-HOLM: " + Client.QUADE(path, Client.SIGNIFICANCE_LEVEL[0], POST_HOC.HOLM));
-        System.out.println("QUADE-FINNER: " + Client.QUADE(path, Client.SIGNIFICANCE_LEVEL[0], POST_HOC.FINNER));
-        System.out.println("QUADE-HOCHBERG: " + Client.QUADE(path, Client.SIGNIFICANCE_LEVEL[0], POST_HOC.HOCHBERG));
+        System.out.println("Kolmogorov-Smirnov: " + StacConsumer.KOLMOGOROV(path, StacConsumer.SIGNIFICANCE_LEVEL[3]));
+        System.out.println("Agostino: " + StacConsumer.AGOSTINO(path, StacConsumer.SIGNIFICANCE_LEVEL[3]));
+        System.out.println("Shapiro: " + StacConsumer.SHAPIRO(path, StacConsumer.SIGNIFICANCE_LEVEL[3]));
+        System.out.println("Friedman: " + StacConsumer.FRIEDMAN(path, StacConsumer.SIGNIFICANCE_LEVEL[3], POST_HOC.HOLM));
+        System.out.println("Friedman Aligned Ranks: " + StacConsumer.FRIEDMAN_ALIGNED_RANK(path, StacConsumer.SIGNIFICANCE_LEVEL[1], POST_HOC.HOCHBERG));
+        System.out.println("QUADE-NEMENYI: " + StacConsumer.QUADE(path, StacConsumer.SIGNIFICANCE_LEVEL[0], POST_HOC.NEMENYI));
+        System.out.println("QUADE-HOLM: " + StacConsumer.QUADE(path, StacConsumer.SIGNIFICANCE_LEVEL[0], POST_HOC.HOLM));
+        System.out.println("QUADE-FINNER: " + StacConsumer.QUADE(path, StacConsumer.SIGNIFICANCE_LEVEL[0], POST_HOC.FINNER));
+        System.out.println("QUADE-HOCHBERG: " + StacConsumer.QUADE(path, StacConsumer.SIGNIFICANCE_LEVEL[0], POST_HOC.HOCHBERG));
 
-        System.out.println("ANOVA: " + Client.ANOVA(path, Client.SIGNIFICANCE_LEVEL[3]));
+        System.out.println("ANOVA: " + StacConsumer.ANOVA(path, StacConsumer.SIGNIFICANCE_LEVEL[3]));
         System.out.println("ANOVA WITHIN CASES: ");
-        Map<String, Object> map = Client.ANOVA_WITHIN(path, Client.SIGNIFICANCE_LEVEL[0]);
+        Map<String, Object> map = StacConsumer.ANOVA_WITHIN(path, StacConsumer.SIGNIFICANCE_LEVEL[0]);
         map.forEach((k, v) -> System.out.println(k + " : " + v));
 
 
--- src/main/java/StacConsumer.java
+++ src/main/java/StacConsumer.java
@@ -17,8 +17,8 @@ import java.util.Map;
 /**
  * Example of consume web service from STAC Web Platform
  */
-public class Client {
-    public static final double[] SIGNIFICANCE_LEVEL = {0.10, 0.05, 0.02, 0.01, 0.005, 0.001};
+public class StacConsumer {
+    public static final double[] SIGNIFICANCE_LEVEL = { 0.10, 0.05, 0.02, 0.01, 0.005, 0.001 };
     private static final String BASE_URL = "https://tec.citius.usc.es/stac/api/";
     private static final Gson gson = new GsonBuilder().create();
     private static final HttpClient httpClient = HttpClientBuilder.create().build();
@@ -34,8 +34,9 @@ public class Client {
      * <p>
      *
      * @param path_csv_file      load data
-     * @param significance_level Probability of rejecting a null hypothesis when it is true.
-     *                           Also known as confidence level or Type I error (false positive)
+     * @param significance_level Probability of rejecting a null hypothesis when it
+     *                           is true. Also known as confidence level or Type I
+     *                           error (false positive)
      * @return Map object response
      */
     public static Map<String, Object> AGOSTINO(String path_csv_file, double significance_level) {
@@ -46,7 +47,8 @@ public class Client {
     /**
      * <bold>Shapiro-Wilk</bold>
      * <p>
-     * The best of the three methods, especially for samples of less than 30 elements.
+     * The best of the three methods, especially for samples of less than 30
+     * elements.
      * <p>
      * <bold>Type: Parametric Test</bold>
      * <p>
@@ -54,15 +56,15 @@ public class Client {
      * <p>
      *
      * @param path_csv_file      load data
-     * @param significance_level Probability of rejecting a null hypothesis when it is true.
-     *                           Also known as confidence level or Type I error (false positive)
+     * @param significance_level Probability of rejecting a null hypothesis when it
+     *                           is true. Also known as confidence level or Type I
+     *                           error (false positive)
      * @return Map object response
      */
     public static Map<String, Object> SHAPIRO(String path_csv_file, double significance_level) {
         String end_point = "shapiro/" + significance_level;
         return getMap(path_csv_file, end_point);
     }
-
 
     /**
      * <bold>Kolmogorov-Smirnov</bold>
@@ -75,8 +77,9 @@ public class Client {
      * <p>
      *
      * @param path_csv_file      load data
-     * @param significance_level Probability of rejecting a null hypothesis when it is true.
-     *                           Also known as confidence level or Type I error (false positive)
+     * @param significance_level Probability of rejecting a null hypothesis when it
+     *                           is true. Also known as confidence level or Type I
+     *                           error (false positive)
      * @return Map object response
      */
     public static Map<String, Object> KOLMOGOROV(String path_csv_file, double significance_level) {
@@ -87,8 +90,10 @@ public class Client {
     /**
      * <bold>ANOVA between cases</bold>
      * <p>
-     * Tests the null hypothesis that the means of the results of two or more groups are the same. For this, the test
-     * analyzes the variation between samples as well as their inner variation with the variance. The statistic of the ANOVA test, is estimated by the f-distribution.
+     * Tests the null hypothesis that the means of the results of two or more groups
+     * are the same. For this, the test analyzes the variation between samples as
+     * well as their inner variation with the variance. The statistic of the ANOVA
+     * test, is estimated by the f-distribution.
      * <p>
      * <bold>Type:Parametric Test</bold>
      * <p>
@@ -96,8 +101,9 @@ public class Client {
      * <p>
      *
      * @param path_csv_file      load data
-     * @param significance_level Probability of rejecting a null hypothesis when it is true.
-     *                           Also known as confidence level or Type I error (false positive)
+     * @param significance_level Probability of rejecting a null hypothesis when it
+     *                           is true. Also known as confidence level or Type I
+     *                           error (false positive)
      * @return Map object response
      */
     public static Map<String, Object> ANOVA(String path_csv_file, double significance_level) {
@@ -107,8 +113,10 @@ public class Client {
     /**
      * <bold>ANOVA WITHIN CASES</bold>
      * <p>
-     * Tests the null hypothesis that the means of the results of two or more groups are the same. For this, the test
-     * analyzes the variation between samples as well as their inner variation with the variance. The statistic of the ANOVA test, is estimated by the f-distribution.
+     * Tests the null hypothesis that the means of the results of two or more groups
+     * are the same. For this, the test analyzes the variation between samples as
+     * well as their inner variation with the variance. The statistic of the ANOVA
+     * test, is estimated by the f-distribution.
      * <p>
      * <bold>Type:Parametric Test</bold>
      * <p>
@@ -116,20 +124,21 @@ public class Client {
      * <p>
      *
      * @param path_csv_file      load data
-     * @param significance_level Probability of rejecting a null hypothesis when it is true.
-     *                           Also known as confidence level or Type I error (false positive)
+     * @param significance_level Probability of rejecting a null hypothesis when it
+     *                           is true. Also known as confidence level or Type I
+     *                           error (false positive)
      * @return Map object response
      */
     public static Map<String, Object> ANOVA_WITHIN(String path_csv_file, double significance_level) {
         return getMap(path_csv_file, "anova-within/" + significance_level);
     }
 
-
     /**
      * <bold>Friedman</bold>
      * <p>
-     * This test makes comparisons and assigns rankings to each data set. The statistic follows a chis-quared
-     * distribution with K−1 degrees of freedom, being K the number of related variables (or number of algorithms).
+     * This test makes comparisons and assigns rankings to each data set. The
+     * statistic follows a chis-quared distribution with K−1 degrees of freedom,
+     * being K the number of related variables (or number of algorithms).
      * <p>
      * <bold>Type: Non Parametric Test</bold>
      * <p>
@@ -137,8 +146,9 @@ public class Client {
      * <p>
      *
      * @param path_csv_file      load data
-     * @param significance_level Probability of rejecting a null hypothesis when it is true.
-     *                           Also known as confidence level or Type I error (false positive)
+     * @param significance_level Probability of rejecting a null hypothesis when it
+     *                           is true. Also known as confidence level or Type I
+     *                           error (false positive)
      * @param post_hoc           Post-hoc multiple comparison
      * @return Map object response
      */
@@ -149,8 +159,8 @@ public class Client {
     /**
      * <bold>Friedman Aligned Ranks</bold>
      * <p>
-     * It makes comparisons an assigns rankings considering all the data sets. It is usually employed when the number
-     * of algorithms in the comparison is low
+     * It makes comparisons an assigns rankings considering all the data sets. It is
+     * usually employed when the number of algorithms in the comparison is low
      * <p>
      * <bold>Type: Non Parametric Test</bold>
      * <p>
@@ -158,21 +168,23 @@ public class Client {
      * <p>
      *
      * @param path_csv_file      load data
-     * @param significance_level Probability of rejecting a null hypothesis when it is true.
-     *                           Also known as confidence level or Type I error (false positive)
+     * @param significance_level Probability of rejecting a null hypothesis when it
+     *                           is true. Also known as confidence level or Type I
+     *                           error (false positive)
      * @param post_hoc           Post-hoc multiple comparison
      * @return Map object response
      */
-    public static Map<String, Object> FRIEDMAN_ALIGNED_RANK(String path_csv_file, double significance_level, POST_HOC post_hoc) {
+    public static Map<String, Object> FRIEDMAN_ALIGNED_RANK(String path_csv_file, double significance_level,
+            POST_HOC post_hoc) {
         return getMap(path_csv_file, "friedman-aligned-ranks/" + post_hoc + "/" + significance_level);
     }
-
 
     /**
      * <bold>Quade</bold>
      * <p>
-     * Similar to ImanDavenport, only that it takes into account that some problems are more difficult or that the
-     * results obtained from different algorithms present higher discrepancies (weighting).
+     * Similar to ImanDavenport, only that it takes into account that some problems
+     * are more difficult or that the results obtained from different algorithms
+     * present higher discrepancies (weighting).
      * <p>
      * <bold>Type: Non Parametric Test</bold>
      * <p>
@@ -180,8 +192,9 @@ public class Client {
      * <p>
      *
      * @param path_csv_file      load data
-     * @param significance_level Probability of rejecting a null hypothesis when it is true.
-     *                           Also known as confidence level or Type I error (false positive)
+     * @param significance_level Probability of rejecting a null hypothesis when it
+     *                           is true. Also known as confidence level or Type I
+     *                           error (false positive)
      * @param post_hoc           Post-hoc multiple comparison
      * @return Map object response
      */
@@ -189,7 +202,29 @@ public class Client {
         return getMap(path_csv_file, "quade/" + post_hoc + "/" + significance_level);
     }
 
-    @SuppressWarnings({"rawtypes", "unchecked"})
+    /**
+     * <bold>Wilcoxon</bold>
+     * <p>
+     * paired data. Assumes that the differences between samples are symmetrical
+     * with respect to the median.
+     * <p>
+     * <bold>Type: Non Parametric Test Two groups</bold>
+     * <p>
+     * <bold>Subtype: Normality Test</bold>
+     * <p>
+     *
+     * @param path_csv_file      load data
+     * @param significance_level Probability of rejecting a null hypothesis when it
+     *                           is true. Also known as confidence level or Type I
+     *                           error (false positive)
+     * @return Map object response
+     */
+    public static Map<String, Object> WILCOXON(String path_csv_file, double significance_level) {
+        String end_point = "wilcoxon/" + significance_level;
+        return getMap(path_csv_file, end_point);
+    }
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
     private static String transform_to_json(String path_csv_file) throws IOException {
         Table table = Table.read().csv(path_csv_file);
         HashMap<String, ArrayList> data = new HashMap<>();
@@ -217,6 +252,5 @@ public class Client {
         }
         return null;
     }
-
 
 }
